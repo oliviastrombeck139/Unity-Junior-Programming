cb0bfe59505cad22e3faddf8d38049f8a98635e9
oliviastrombeck <strombeckolivia@gmail.com>
2026-09-22 08:39:33 -0400

    Start of 1.3.1

--- Prototype 1/Assets/Scripts/PlayerControler.cs
+++ Prototype 1/Assets/Scripts/PlayerControler.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    public float speed = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +13,6 @@ public class PlayerControler : MonoBehaviour
     void Update()
     {
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
