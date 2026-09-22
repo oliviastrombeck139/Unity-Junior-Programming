f058b56fd8ef79472dee6be839a2a8474049dce3
oliviastrombeck <strombeckolivia@gmail.com>
2026-09-22 08:33:56 -0400

    1.2 - 9/22/26
    Added more cones and physics to scene, car can move but isn't controlable.

--- Prototype 1/Assets/Scripts/PlayerControler.cs
+++ Prototype 1/Assets/Scripts/PlayerControler.cs
@@ -11,6 +11,7 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
