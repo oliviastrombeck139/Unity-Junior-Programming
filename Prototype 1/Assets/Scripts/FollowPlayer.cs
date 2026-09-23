6fd57648039f1bd1413722ef636ff0e2e0f6f9b0
oliviastrombeck <strombeckolivia@gmail.com>
2026-09-23 07:54:42 -0400

    1.3.3 - 9/23/26
    Fixed camera's position when following the vehicle.

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -12,6 +12,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        // offsets camera behind the player by adding to the player's position
+        transform.position = player.transform.position + new Vector3(0,5,-7);
     }
 }
