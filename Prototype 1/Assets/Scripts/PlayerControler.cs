eaea89b882114a1da422ab504a38487b71c561fa
oliviastrombeck <strombeckolivia@gmail.com>
2026-09-24 08:02:31 -0400

    1.4.5 - 9/24/26

--- Prototype 1/Assets/Scripts/PlayerControler.cs
+++ Prototype 1/Assets/Scripts/PlayerControler.cs
@@ -6,10 +6,12 @@ public class PlayerControler : MonoBehaviour
     public float speed = 10.0f;
     public float turnSpeed;
     public InputAction moveAction;
+    public Vector2 moveInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction.Enable();
     }
 
     // Update is called once per frame
@@ -18,5 +20,6 @@ public class PlayerControler : MonoBehaviour
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        moveInput = moveAction.ReadValue<Vector2>();
     }
 }
