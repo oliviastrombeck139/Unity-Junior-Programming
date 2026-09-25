85e383a9afba8db11b0dfab44d8c050429703690
oliviastrombeck <strombeckolivia@gmail.com>
2026-09-25 07:17:01 -0400

    9/24 - Downloaded Plane Challenge Files

--- Prototype 1/Assets/Scripts/PlayerControler.cs
+++ Prototype 1/Assets/Scripts/PlayerControler.cs
@@ -3,14 +3,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerControler : MonoBehaviour
 {
+    //Movement tuning (editable in inspector)
     public float speed = 10.0f;
     public float turnSpeed;
+    // Input system action esposed in inspector for binding (WASD/arrow keys)
     public InputAction moveAction;
-    public Vector2 moveInput;
+    // Current input value (x = left/right, y = forward/back), kept private for internal use
+    private Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Enable the moveAction so it starts reading input
         moveAction.Enable();
     }
 
@@ -19,10 +23,10 @@ public class PlayerControler : MonoBehaviour
     {
         // We'll move the vehicle forward
 
-        // Moves vehicle forward/back
+        // Moves vehicle forward/back along local Z using the y component
         transform.Translate(Vector3.forward * Time.deltaTime * speed * moveInput.y);
 
-        // Moves vehicle left/right
+        // Rotate around local Y (yaw) using the x component
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * moveInput.x);
 
         moveInput = moveAction.ReadValue<Vector2>();
